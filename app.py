98a8df06f4c960f1a6cf21964fedaaaebfa6518a
Daniel Mesa <danimesamejias@gmail.com>
2021-04-26 19:06:30 +0200

    Añadir programa principal

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template,abort
+import json
+with open("books.json") as fichero:
+    datos=json.load(fichero)
+app = Flask(__name__)	
+
+@app.route('/')
+def inicio():
+    libros=[]
+    for i in datos:
+        dic={"titulo":i.get("title"),"isbn":i.get("isbn")}
+        libros.append(dic)
+    return render_template("inicio.html",libros=libros)
+@app.route('/libro/<num1>')
+def detalle(num1):
+    ind=0
+    for i in datos:
+        if i.get("isbn") == num1:
+            tit=i.get("title")
+            img=i.get("thumbnailUrl")
+            autores=i.get("authors")
+            descripcion=i.get("longDescription")
+            categorias=i.get("categories")
+            status=i.get("status")
+            ind=1
+    if ind == 0:
+        abort(404)
+    return render_template("libro.html",tit=tit,img=img,autores=autores,descripcion=descripcion,categorias=categorias,status=status)
+
+app.run(debug=True)
